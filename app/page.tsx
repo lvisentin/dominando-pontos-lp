30eeb65fc8671865c2bcf737040ec36447335e49
lvisentin <lvise.batista@gmail.com>
2024-07-01 12:56:08 -0300

    v1

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,125 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <main className="max-w-7xl mx-auto">
+        <header className="flex justify-center px-5">
+          <Image src="/assets/logo_dominando.png" width={250} height={50} />
+        </header>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <section className="flex flex-col items-center mb-12">
+          <h1 className="text-4xl font-bold text-center mb-4">Não somos um <span className="blue">grupo de alertas</span></h1>
+          <h2 className="text-xl">cansado de participar de <b>vários grupos</b> com emissões que <b>não te interessam?</b></h2>
+          <button className="blue-btn text-white p-4 rounded-xl font-bold text-xl px-8">Quero viajar barato!</button>
+        </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <section className="flex flex-col items-center mb-12">
+          <h1 className="text-4xl font-bold text-center mb-6">O que <span className="blue">o mercado</span> te oferece</h1>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="card bg-white py-6 bg-white rounded-xl px-2 shadow-lg w-fit">
+            <p className="text-lg font-semibold text-center mb-6">Grupos de alertas</p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            <ul className="flex flex-col px-4 gap-4">
+              <li className="flex"><p className="icon mb-0 mr-2">❌</p> Emissões personalizadas</li>
+              <li className="flex"><p className="icon mb-0 mr-2">❌</p> Destinos que te interessam</li>
+              <li className="flex"><p className="icon mb-0 mr-2">✅</p> Spam no seu Whatsapp</li>
+              <li className="flex"><p className="icon mb-0 mr-2">✅</p> Promoções que você nunca vai usar</li>
+            </ul>
+          </div>
+        </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <section className="flex flex-col items-center mb-12">
+          <h1 className="text-4xl font-bold text-center mb-6">O que a <span className="blue">Dominando pontos</span> te oferece</h1>
+
+          <div className="flex gap-4">
+            <div className="card bg-white py-6 rounded-xl px-2 shadow-lg w-fit">
+              <p className="text-lg font-semibold text-center mb-6">Robô de busca de passagens</p>
+
+              <ul className="flex flex-col px-4 gap-4">
+                <li className="flex"><p className="icon mb-0 mr-2">✅</p> Emissões personalizadas</li>
+                <li className="flex"><p className="icon mb-0 mr-2">✅</p> Destinos que te interessam</li>
+                <li className="flex"><p className="icon mb-0 mr-2">✅</p> Datas personalizadas</li>
+                <li className="flex"><p className="icon mb-0 mr-2">✅</p> Buscas com datas flexíveis</li>
+              </ul>
+            </div>
+
+            <div className="card bg-white py-6 rounded-xl px-2 shadow-lg w-fit">
+              <p className="text-lg font-semibold text-center mb-6">Grupos de alertas</p>
+
+              <ul className="flex flex-col px-4 gap-4">
+                <li className="flex"><p className="icon mb-0 mr-2">✅</p> As melhores promoções</li>
+                <li className="flex"><p className="icon mb-0 mr-2">✅</p> Categorias personalizadas</li>
+                <li className="flex"><p className="icon mb-0 mr-2">✅</p> Ligar e desligar os alertas <br />quando quiser</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+
+        <section className="flex flex-col items-center mb-12">
+          <h1 className="text-4xl font-bold text-center mb-12">Receba alertas dos <span className="blue">principais programas</span> de pontos</h1>
+
+          <div className="cias flex gap-12 items-center">
+            <div className="flex flex-col items-center gap-4">
+              <Image src="/assets/smiles.png" width={250} height={50} />
+              <p className="text-base font-semibold text-center">
+                Smiles
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center gap-4">
+              <Image src="/assets/azul.png" width={250} height={50} />
+              <p className="text-base font-semibold text-center">
+                Azul <span className="blue">(nacional)</span> e <br />
+                Azul pelo mundo <span className="blue">(internacional)</span>
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center gap-4">
+              <Image src="/assets/american.png" width={250} height={50} />
+              <p className="text-base font-semibold text-center">
+                American Airlines
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section className="flex flex-col items-center mb-12">
+          <h1 className="text-4xl font-bold text-center mb-12">Algumas das emissões que <span className="blue">nossos usuários</span> <br /> estão tendo acesso</h1>
+
+          <div className="emissoes flex items-center gap-y-6 flex-wrap">
+            <div className="emissao flex flex-col items-center w-1/3">
+              <Image className="rounded-3xl" src="/assets/emissao1.jpeg" width={300} height={315} />
+              <p className="mt-4 text-2xl font-bold text-center">Orlando por <br /> <span className="blue">R$1200</span></p>
+            </div>
+
+            <div className="emissao flex flex-col items-center w-1/3">
+              <Image className="rounded-3xl" src="/assets/emissao1.jpeg" width={300} height={315} />
+              <p className="mt-4 text-2xl font-bold text-center">Orlando por <br /> <span className="blue">R$1200</span></p>
+            </div>
+
+            <div className="emissao flex flex-col items-center w-1/3">
+              <Image className="rounded-3xl" src="/assets/emissao1.jpeg" width={300} height={315} />
+              <p className="mt-4 text-2xl font-bold text-center">Orlando por <br /> <span className="blue">R$1200</span></p>
+            </div>
+
+            <div className="emissao flex flex-col items-center w-1/3">
+              <Image className="rounded-3xl" src="/assets/emissao1.jpeg" width={300} height={315} />
+              <p className="mt-4 text-2xl font-bold text-center">Orlando por <br /> <span className="blue">R$1200</span></p>
+            </div>
+
+            <div className="emissao flex flex-col items-center w-1/3">
+              <Image className="rounded-3xl" src="/assets/emissao1.jpeg" width={300} height={315} />
+              <p className="mt-4 text-2xl font-bold text-center">Orlando por <br /> <span className="blue">R$1200</span></p>
+            </div>
+
+            <div className="emissao flex flex-col items-center w-1/3">
+              <Image className="rounded-3xl" src="/assets/emissao1.jpeg" width={300} height={315} />
+              <p className="mt-4 text-2xl font-bold text-center">Orlando por <br /> <span className="blue">R$1200</span></p>
+            </div>
+
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
